66a815d078b666bb21466adbceef994b330559cf
Matteo <matteo.dinville@ynov.com>
2021-11-16 14:42:46 +0100

    join albums-tracks

--- join-albums-tracks.sql
+++ join-albums-tracks.sql
@@ -1,3 +1,3 @@
 SELECT Title AS AlbumName, Name AS TrackName, Milliseconds
 FROM albums 
-INNER JOIN tracks ON tracks.AlbumId=albums.AlbumId ORDER BY Milliseconds ASC;
+INNER JOIN tracks ON tracks.AlbumId=albums.AlbumId ORDER BY Milliseconds ASC LIMIT 50;
