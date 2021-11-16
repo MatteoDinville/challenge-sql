0f278780f89736e70c6afba2651baab3f2f247b2
Matteo <matteo.dinville@ynov.com>
2021-11-16 15:21:24 +0100

    double

--- double-join.sql
+++ double-join.sql
@@ -1,6 +1,6 @@
 SELECT DISTINCT tracks."Name" AS TrackName, playlists."Name" AS PlayslistName
 FROM playlist_track
-INNER JOIN playlist ON playlists.PlaylistId=playlist_track.PlaylistId
+INNER JOIN playlists ON playlists.PlaylistId=playlist_track.PlaylistId
 INNER JOIN tracks ON tracks.TrackId=playlist_track.TrackId
 WHERE playlists."Name" = 'TV Shows'
 LIMIT 100;
