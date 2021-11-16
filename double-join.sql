fd0ad9af30ce43a54f143153ab46907595b10e3a
Matteo <matteo.dinville@ynov.com>
2021-11-16 15:32:12 +0100

    double

--- double-join.sql
+++ double-join.sql
@@ -1,4 +1,4 @@
-SELECT DISTINCT tracks."Name" AS TrackName, playlists."Name" AS PlayslistName
+SELECT DISTINCT tracks."Name" AS TrackName, playlists."Name" AS PlaylistName
 FROM playlists
 INNER JOIN playlist_track ON playlists.PlaylistId=playlist_track.PlaylistId
 INNER JOIN tracks ON tracks.TrackId=playlist_track.TrackId
