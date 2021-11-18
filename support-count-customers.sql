fcb5d9f200905f53ee3470f89c56d075b7d67bfc
Matteo <matteo.dinville@ynov.com>
2021-11-18 17:26:12 +0100

    count customers

---
+++ support-count-customers.sql
@@ -0,0 +1,6 @@
+SELECT UPPER(employees.FirstName) || ' ' || UPPER(employees.LastName) AS FullName, COUNT(customers.CustomerId) AS NumberOfCustomers
+FROM employees
+INNER JOIN customers ON employees.EmployeeId = customers.SupportRepId
+WHERE employees.Title = 'Sales Support Agent'
+GROUP BY employees.EmployeeId
+ORDER BY NumberOfCustomers ASC
