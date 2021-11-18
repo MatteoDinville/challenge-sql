c13cc4000c47b983a6e1a8a05c5ebdfe63eb48fb
Matteo <matteo.dinville@ynov.com>
2021-11-18 23:29:00 +0100

    total invoices

--- total-invoices.sql
+++ total-invoices.sql
@@ -1,7 +1,7 @@
 SELECT customers.FirstName || ' ' || UPPER(customers.LastName) AS FullName, SUM(UnitPrice) AS AllInvoices
 FROM invoices
-INNER JOIN customers ON invoices.CustomerId= customers.CustomerId
 INNER JOIN invoice_items ON invoice_items.InvoiceId= invoices.InvoiceId
+INNER JOIN customers ON invoices.CustomerId= customers.CustomerId
 GROUP BY customers.CustomerId
 HAVING AllInvoices > 38
 ORDER BY FirstName
