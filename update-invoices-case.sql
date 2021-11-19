cf5f7feeba0da214e1bb59bfc6b5a404531aae38
Matteo <matteo.dinville@ynov.com>
2021-11-19 15:07:34 +0100

    update

--- update-invoices-case.sql
+++ update-invoices-case.sql
@@ -1,8 +1,8 @@
-UPDATE invoices
+UPDATE `invoices`
 SET Total = (
 CASE 
-	WHEN BillingCountry = 'FRANCE' THEN 'Total' + 3
-	WHEN BillingCountry = 'USA' THEN 'Total' + 5
+	WHEN `BillingCountry`= 'FRANCE' THEN 'Total' + 3
+	WHEN `BillingCountry` = 'USA' THEN 'Total' + 5
 	ELSE 'Total' + 1
 END
 )
