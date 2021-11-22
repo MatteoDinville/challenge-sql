d590e9eef61ed61b971192aa19ef9c358b41ffcf
Matteo <matteo.dinville@ynov.com>
2021-11-22 15:11:07 +0100

    invoices

---
+++ invoices-items-last.sql
@@ -0,0 +1,11 @@
+SELECT Name
+FROM tracks 
+WHERE "TrackId" = ( 
+    SELECT "TrackId"
+    FROM invoice_items
+    WHERE "InvoiceId" = (
+        SELECT MAX("InvoiceId")
+        FROM invoices
+        )
+    LIMIT 1
+)
